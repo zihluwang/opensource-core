369f0f87f3e76c2668c54635c3089a9e5efedbe8
theodorehills <theodore.hills@outlook.com>
2022-03-02 00:01:31 +0800

    refactor: Simplified the process to throw an exception.

--- src/main/java/cn/vorbote/core/exceptions/TimeOutRangeException.java
+++ src/main/java/cn/vorbote/core/exceptions/TimeOutRangeException.java
@@ -1,16 +1,48 @@
 package cn.vorbote.core.exceptions;
 
 /**
- * Time value out of its range exception.
+ * If the value of the specific field in time is out of its correct range, this exception will be thrown.<br/>
+ * For example, if you set the field "minute" to 60 will cause this exception.
  *
  * @author vorbote
  */
 public class TimeOutRangeException extends RuntimeException {
 
-    public TimeOutRangeException() {
+    /**
+     * Message of this exception, to tell the coder which field is wrong.
+     */
+    private final String message;
+
+    /**
+     * Constructor, to build a clear exception message.
+     *
+     * @param field               The field which is set to be a wrong value.
+     * @param correctMinimumValue The correct minimum value of this field.
+     * @param correctMaximumValue The correct maximum value of this field.
+     */
+    public TimeOutRangeException(String field, Integer correctMinimumValue, Integer correctMaximumValue) {
+        message = String.format("The %s is out of range of (%d ~ %d).", field, correctMinimumValue,
+                correctMaximumValue);
     }
 
-    public TimeOutRangeException(String message) {
-        super(message);
+    /**
+     * Convert to a {@code String} to make this exception clear enough for coders.
+     *
+     * @return A {@code String} includes what went wrong.
+     */
+    @Override
+    public String toString() {
+        return message;
+    }
+
+    /**
+     * Convert to a {@code String} to make this exception clear enough for coders.
+     *
+     * @return A {@code String} includes what went wrong.
+     * @see #toString()
+     */
+    @Override
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/cn/vorbote/core/exceptions/UnsupportedHashAlgorithmException.java
+++ src/main/java/cn/vorbote/core/exceptions/UnsupportedHashAlgorithmException.java
@@ -7,11 +7,20 @@ package cn.vorbote.core.exceptions;
  * @since 3.0.0
  */
 public class UnsupportedHashAlgorithmException extends RuntimeException {
-    public UnsupportedHashAlgorithmException() {
-        super();
+
+    private final String message;
+
+    public UnsupportedHashAlgorithmException(String algorithm) {
+        message = String.format("Unsupported algorithm %s.", algorithm);
     }
 
-    public UnsupportedHashAlgorithmException(String message) {
-        super(message);
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
     }
 }
